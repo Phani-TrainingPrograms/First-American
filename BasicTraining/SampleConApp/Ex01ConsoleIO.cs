3ed01e53cc6790e4d9336d53c8c87ad87e95c985
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2023-04-18 13:26:07 +0530

    Add files via upload

---
+++ BasicTraining/SampleConApp/Ex01ConsoleIO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConApp
+{
+    class Ex01ConsoleIO
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Name");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Enter the Age");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("Enter UR College");
+            var college = Console.ReadLine();
+
+            var outputData = $"The Name entered is {name} studying in {college} with Mr.{name}'s age {age}";
+            //Older syntax of string literal used with variable data. 
+            outputData = string.Format("The Name entered is {0} studying in {1} and Mr.{0}'s age is {2}", name, college, age, "The name entered is ");
+            Console.WriteLine(outputData);
+        }
+    }
+}
+//Todo: Explore on Read and ReadKey methods of the Console class and observe its differences and practical usages of these APIs. 
---
+++ BasicTraining/SampleConApp/Ex02DataTypes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SampleConApp
+{
+    class Ex02DataTypes
+    {
+        static void Main(string[] args)
+        {
+            //Syntax is similar to C++...
+            int iValue = 123;
+            double dValue = 3245.56;
+            float fValue = 345.46f;//use the suffix f for float, else it will be double. 
+            short sValue = 345;
+            DateTime dt = DateTime.Now;
+            Console.WriteLine("The integer is " + iValue);
+            Console.WriteLine("The double is " + dValue);
+            Console.WriteLine("The Float is " + fValue);
+            Console.WriteLine("The Short is " + sValue);
+            Console.WriteLine("The Current Date is " + dt);
+
+            Console.WriteLine($"The range of byte is {byte.MinValue} and {byte.MaxValue}");
+            Console.WriteLine($"The range of short is {short.MinValue} and {short.MaxValue}");
+            Console.WriteLine($"The range of int is {int.MinValue} and {int.MaxValue}");
+            Console.WriteLine($"The range of long is {long.MinValue} and {long.MaxValue}");
+            Console.WriteLine($"The range of float is {float.MinValue} and {float.MaxValue}");
+            Console.WriteLine($"The range of double is {double.MinValue} and {double.MaxValue}"); 
+            Console.WriteLine($"The range of decimal is {decimal.MinValue} and {decimal.MaxValue}");
+            Console.WriteLine($"The range of Bool is true and false");
+
+            string intValue = iValue.ToString();//ToString is a function of the base class of all Data types called object which converts the type to String representation. 
+            Console.WriteLine(intValue + " converted as string");
+
+            //////////Data Conversions/////
+            double dVal = Convert.ToDouble(123);
+            iValue = 123;
+            dVal = iValue;//double is larger than int..
+            iValue = (int)dVal;
+        }
+    }
+}
