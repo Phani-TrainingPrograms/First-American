808376f7b02735624fae120078d836ca50cdfe1d
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2023-04-18 11:17:58 +0530

    Add files via upload

---
+++ BasicTraining/SampleConApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SampleConApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Time for break!!!!");
+            //Execute: Ctrl+ F5
+        }
+    }
+}
