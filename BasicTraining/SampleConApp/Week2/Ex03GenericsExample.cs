e281ea839d9523c137b74088e8e20418a5d3d692
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2023-04-24 16:31:35 +0530

    Add files via upload

---
+++ BasicTraining/SampleConApp/Week2/Ex03GenericsExample.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace SampleConApp.Week2
+{
+    class Ex03GenericsExample
+    {
+        static void Main(string[] args)
+        {
+            //queueExample();
+            //stackExample();
+            sortedListExample();
+        }
+
+        private static void sortedListExample()
+        {
+            SortedList<string, int> data = new SortedList<string, int>();
+            data.Add("Apples", 5);
+            data.Add("Mangoes", 15);
+            data.Add("Oranges", 4);
+            data.Add("Bananas", 8);
+            data.Add("Kiwi Fruits", 4);
+            foreach(var item in data)
+            {
+                Console.WriteLine($"{item.Key}-{item.Value}");
+                //Output comes in the sorted order of the keys...
+            }
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                Console.WriteLine(data.Keys[i]);
+            }
+            //Todo: Create an Example on SortedDictionary and see how it behaves. Refer the MSDN and note the differences of SortedList vs. SortedDictionary.
+            //
+            SortedDictionary<string, int> data2 = new SortedDictionary<string, int>(data);
+            foreach(var pair in data2)
+                Console.WriteLine(pair.Value);
+        }
+
+        private static void stackExample()
+        {
+            Stack<String> pages = new Stack<string>();
+            int i = 0;
+            do
+            {
+                pages.Push(UIConsole.GetString("Enter the Page to View"));
+                foreach (var page in pages) Console.WriteLine(page);
+                Console.WriteLine("\n\n");
+                i++;
+            } while (i < 2);
+            //Remove the pages from the Stack
+            i = pages.Count;
+            do
+            {
+                --i;
+                Console.WriteLine(pages.Pop() + " is removed");//Removes the Top page from the phone...
+            } while (i > 0);
+        }
+
+        private static void queueExample()
+        {
+            Queue<string> recentList = new Queue<string>();
+            do
+            {
+                var input = UIConsole.GetString("Enter the Item to view");
+                if (recentList.Count == 5)
+                    recentList.Dequeue();//Removes the 1st item in the Collection
+                recentList.Enqueue(input);//Adds the item to the bottom of the collection. 
+                displayQueueItems(recentList);
+            } while (true);
+        }
+
+        private static void displayQueueItems(Queue<string> recentList)
+        {
+            Console.WriteLine("The Recent List of Items:");
+            var list = recentList.Reverse();
+            foreach(var item in list)
+                Console.WriteLine(item);            
+        }
+    }
+}
---
+++ BasicTraining/SampleConApp/Week2/Ex04SortingItems.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Sorting internally looks for an interface called IComparable<T> or IComparer<T>. 
+namespace SampleConApp.Week2
+{
+    class Ex04SortingItems
+    {
+        static void Main(string[] args)
+        {
+            List<Item> items = new List<Item>();
+            items.Add(new Item { ItemName = "Samsung TV", ItemPrice = 65000 });
+            items.Add(new Item { ItemName = "Apple TV", ItemPrice = 145000 });
+            items.Add(new Item { ItemName = "MI TV", ItemPrice = 35000 });
+            items.Add(new Item { ItemName = "Onida TV", ItemPrice = 25000 });
+            items.Add(new Item { ItemName = "Sony TV", ItemPrice = 90000 });
+            items.Add(new Item { ItemName = "Oppo TV", ItemPrice = 50000 });
+
+            string choice = UIConsole.GetString("Select 1 for sorting by Name or 2 for Price");
+            if (choice == "1")
+            {
+                items.Sort();//Will call the IComparable's CompareTo method....
+            }
+            else if (choice == "2")
+            {
+                items.Sort(new ItemComparer(ItemComparingCriteria.Price));
+            }
+            foreach (var item in items)
+                Console.WriteLine(item.ItemName + "====>" + item.ItemPrice);
+
+        }
+    }
+}
+
