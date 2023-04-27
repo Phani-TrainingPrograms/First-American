165dc7d4a6df5d1a068db5323ea718c5140236ff
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2023-04-27 12:13:23 +0530

    Add files via upload

---
+++ BasicTraining/SampleDataAccessLib/MathClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDataAccessLib
+{
+    /// <summary>
+    /// This class contains the APIs for performing simple Arithematic Operations required for BasicTraining Application
+    /// </summary>
+    public class MathClass
+    {
+        public static double AddFunc(double v1, double v2) => v1 + v2;
+        public static double SubFunc(double v1, double v2) => v1 - v2;
+        public static double MulFunc(double v1, double v2) => v1 * v2;
+        public static double DivFunc(double v1, double v2) => v1 / v2;
+    }
+}
